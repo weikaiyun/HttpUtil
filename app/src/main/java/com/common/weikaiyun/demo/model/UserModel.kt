573a1587041aa41bb472130ed4854bcfb5424816
weikaiyun <wky19900505@gmail.com>
2020-10-08 11:18:54 +0800

    优化

--- app/src/main/java/com/common/weikaiyun/demo/model/UserModel.kt
+++ app/src/main/java/com/common/weikaiyun/demo/model/UserModel.kt
@@ -5,8 +5,6 @@ import com.common.weikaiyun.demo.db.UserDatabase
 import com.common.weikaiyun.retrofit.safecall.SafeApiRequest
 
 class UserModel: SafeApiRequest() {
-    suspend fun getAll() = UserDatabase.getInstance().userDao().getAll()
-
     suspend fun insertAll(vararg users: User) = UserDatabase.getInstance().userDao().insertAll(*users)
 
     suspend fun updateAll(vararg users: User) = UserDatabase.getInstance().userDao().updateAll(*users)
