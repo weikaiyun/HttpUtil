88ce229f225b8ce95e2fb2e54cfc92dfb7114793
weikaiyun <wky19900505@gmail.com>
2020-10-08 14:13:56 +0800

    优化

--- app/src/main/java/com/common/weikaiyun/retrofit/okhttp/CustomInterceptor.kt
+++ app/src/main/java/com/common/weikaiyun/retrofit/okhttp/CustomInterceptor.kt
@@ -1,24 +1,15 @@
 package com.common.weikaiyun.retrofit.okhttp
 
-import android.text.TextUtils
-import android.webkit.CookieManager
 import com.common.weikaiyun.retrofit.NetWorkManager
 import com.common.weikaiyun.retrofit.header.CustomHeader
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class CustomInterceptor: Interceptor {
-    private val cookieManager = CookieManager.getInstance()
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
-        val curUrl = request.url
-        val url = curUrl.toString()
-        val cookie = cookieManager.getCookie(url)
         val builder = request.newBuilder()
-        if (!TextUtils.isEmpty(cookie)) {
-            builder.header("Cookie", cookie)
-        }
 
         NetWorkManager.headerGenerator?.apply {
             val headers: List<CustomHeader> = generateHeaders()
@@ -28,16 +19,7 @@ class CustomInterceptor: Interceptor {
         }
 
         val realRequest = builder.build()
-        val originalResponse = chain.proceed(realRequest)
 
-        /*
-         * -----------------↓↓↓ RESPONSE ↓↓↓-----------------
-         */
-        val headers = originalResponse.headers("Set-Cookie")
-        for (header in headers) {
-            cookieManager.setCookie(originalResponse.request.url.toString(), header)
-        }
-
-        return originalResponse
+        return chain.proceed(realRequest)
     }
 }
