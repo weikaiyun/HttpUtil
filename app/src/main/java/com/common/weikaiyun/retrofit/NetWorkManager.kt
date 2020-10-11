74efc82307f46b32e91acf46e41027bd965797ff
weikaiyun <wky19900505@gmail.com>
2020-10-11 12:12:14 +0800

    优化

--- app/src/main/java/com/common/weikaiyun/retrofit/NetWorkManager.kt
+++ app/src/main/java/com/common/weikaiyun/retrofit/NetWorkManager.kt
@@ -9,7 +9,6 @@ object NetWorkManager {
     val commonHeaders = ArrayList<CustomHeader>()
     private val client = OkHttpClientManager.okHttpClient
 
-    //此处可以使用多url，只要每次baseUrl不同即可
     private fun retrofit(baseUrl : String) : Retrofit = Retrofit.Builder()
             .client(client)
             .baseUrl(baseUrl)
